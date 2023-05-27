64ab9d5b72c43fe9d3cfd0e13595d3640d5e245b
Abdessattar NOISSI <abdessattar.noi@gmail.com>
2023-05-27 23:17:11 +0200

    Functional interface vs Anonyme

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,36 +2,31 @@ package com.example.demo;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.*;
-
 @SpringBootApplication
 public class DemoApplication {
+    @FunctionalInterface
+    interface Formule {
+        double calculer(int a);
 
-    class DemoUtil<T> {
-        public T concat (T obj1, T obj2) {
-            if(obj1 instanceof  String && obj2 instanceof String){
-                String objString1 = (String) obj1;
-                String objString2 = (String) obj2;
-                String result = objString1.concat(objString2);
-                // System.out.println(result);
-                return (T)result;
-            }
-
-            if(obj1 instanceof  Student && obj2 instanceof Student){
-                String objString1 = ((Student) obj1).name;
-                String objString2 = ((Student) obj2).name;
-
-                Student student = new Student(objString1+ " " + objString2);
-                // System.out.println(student);
-                return (T)student;
-            }
-            return null;
+        default double sqrt(int a) {
+            return Math.sqrt(a);
         }
     }
 
     public void test() {
-        DemoUtil<Integer> demoUtil = new DemoUtil<>();
-        System.out.println(demoUtil.concat(Integer.valueOf(1), Integer.valueOf(2)));
+        Formule formule = new Formule() {
+            @Override
+            public double calculer(int a) {
+                return sqrt(a);
+            }
+        };
+
+        // ==>
+
+
+        Formule formule1 = (int a) -> formule.sqrt(a);
+
+        System.out.println(formule1.calculer(144));
     }
 
     public static void main(String[] args) {
