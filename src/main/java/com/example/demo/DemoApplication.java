4dd69ef4a38275b46bfff3928e50c41d3013d711
Abdessattar NOISSI <abdessattar.noi@gmail.com>
2023-05-27 23:36:22 +0200

    sans parenthse

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -13,7 +13,7 @@ public class DemoApplication {
     public void test() {
 
 
-        Formule formule1 = (int a) -> a + 1 ;
+        Formule formule1 = a -> a + 1 ;
 
         System.out.println("méthode lamda1: "+ formule1.calculer(144));
         System.out.println("méthode lamda2: "+ ((Formule)((int a) -> a-154)).calculer(144).compareTo(2));
