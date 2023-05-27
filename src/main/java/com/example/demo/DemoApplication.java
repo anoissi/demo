1aca2f3a7215ca5806b8f587b3fc362e67315d4d
Abdessattar NOISSI <abdessattar.noi@gmail.com>
2023-05-27 22:58:07 +0200

    Type générique

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,29 +7,31 @@ import java.util.*;
 @SpringBootApplication
 public class DemoApplication {
 
-    interface Formule {
-        double calculer(int a);
+    class DemoUtil<T> {
+        public T concat (T obj1, T obj2) {
+            if(obj1 instanceof  String && obj2 instanceof String){
+                String objString1 = (String) obj1;
+                String objString2 = (String) obj2;
+                String result = objString1.concat(objString2);
+                // System.out.println(result);
+                return (T)result;
+            }
 
-        double multipluer(int a, int b);
+            if(obj1 instanceof  Student && obj2 instanceof Student){
+                String objString1 = ((Student) obj1).name;
+                String objString2 = ((Student) obj2).name;
 
-        default double sqrt(int a) {
-            return Math.sqrt(a);
+                Student student = new Student(objString1+ " " + objString2);
+                // System.out.println(student);
+                return (T)student;
+            }
+            return null;
         }
     }
 
     public void test() {
-        Formule formule = new Formule() {
-            @Override
-            public double calculer(int a) {
-                return sqrt(a);
-            }
-
-            @Override
-            public double multipluer(int a, int b) {
-                return 0;
-            }
-        };
-        System.out.println(formule.calculer(144));
+        DemoUtil<Integer> demoUtil = new DemoUtil<>();
+        System.out.println(demoUtil.concat(Integer.valueOf(1), Integer.valueOf(2)));
     }
 
     public static void main(String[] args) {
