c57b2cb17136962f463dc916cbb4759b64a2a307
Abdessattar NOISSI <abdessattar.noi@gmail.com>
2023-05-30 00:13:33 +0200

    static 3

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,25 +8,12 @@ public class DemoApplication {
     public void test() {
         Student student1 = new Student();
         Student student2 = new Student();
-        Student student3 = new Student();
 
-        student1.increment();
-        student2.increment();
-        student3.increment();
-        student1.increment();
+        student1.name = "Abdessattar";
+        student2.name = "Imen";
 
-        int i1 = student1.instanceField;
-        int s1 = Student.staticField;
-
-        int i2 = student2.instanceField;
-        int s2 = Student.staticField;
-
-        int i3 = student3.instanceField;
-        int s3 = Student.staticField;
-
-        System.out.println("student1 => instance : " + i1 + " static: " + s1); // => 3 7
-        System.out.println("student2 => instance : " + i2 + " static: " + s2); // => 2 7
-        System.out.println("student3 => instance : " + i3 + " static: " + s3); // => 2 7
+        System.out.println(student1.name); // ==> Imen
+        System.out.println(student2.name); // ==> Imen
     }
 
     public static void main(String[] args) {
--- src/main/java/com/example/demo/Student.java
+++ src/main/java/com/example/demo/Student.java
@@ -1,16 +1,5 @@
 package com.example.demo;
 
 public class Student {
-
-    int instanceField = 0;
-    static int staticField = 0;
-    public Student() {
-        instanceField++;
-        staticField++;
-    }
-
-    void increment() {
-        instanceField++;
-        staticField++;
-    }
+    static String name ;
 }
